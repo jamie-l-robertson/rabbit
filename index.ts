933679b098043b7a2171742a00e24efa0166f3d9
Jamie Robertson <Jamie.Robertson@shawbrook.co.uk>
2025-01-16 09:35:30 +0000

    added some changes

---
+++ index.ts
@@ -0,0 +1,10 @@
+function adddNumbers(a, b) {
+  return a + b;
+}
+
+const num1 = 5;
+const num2 = 10;
+
+const reslt = adddNumbers(num1, num2);
+
+console.log("The sum of " + num1 + " and " + num2 + " is: " + reslt);
